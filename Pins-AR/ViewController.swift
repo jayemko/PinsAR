14be44229582564ee56c374eba6407c2becea731
Jason Koceja <jason@koceja.com>
2020-10-26 15:21:02 -0700

    Initial Commit

---
+++ Pins-AR/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Pins-AR
+//
+//  Created by Jason Koceja on 10/26/20.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
